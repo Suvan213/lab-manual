cb85e0cba1f99c7b1752f73a687209993baa7126
suvan mahajan <146877853+Suvan213@users.noreply.github.com>
2023-12-12 10:57:11 +0530

    Add files via upload

---
+++ experiment_10.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+void swapByValue(int a, int b) {
+    int temp = a;
+    a = b;
+    b = temp;
+}
+void swapByReference(int *a, int *b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+int main() {
+    int num1 = 5, num2 = 10;
+    printf("Original values: num1 = %d, num2 = %d\n", num1, num2);
+    swapByValue(num1, num2);
+    printf("After swap using pass by value: num1 = %d, num2 = %d\n", num1, num2);
+    swapByReference(&num1, &num2);
+    printf("After swap using pass by reference: num1 = %d, num2 = %d\n", num1, num2);
+
+    return 0;
+}
